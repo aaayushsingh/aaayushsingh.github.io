2646d8428d3644415c5125c4859c32e52a85699a
Ayush Singh <ayushsinghiii.94@gmail.com>
2018-01-20 00:02:07 +0530

    fixed initial block

--- script.js
+++ script.js
@@ -4,37 +4,44 @@ var h1 = document.getElementById('center-div');
 h1.innerText = "Hi!";
 console.log("hello world!");
 
-function test(item){
-    console.log(item);
-}
-test(h1);
-
-function unfade(element) {
-    var op = 0.1;  // initial opacity
-    console.log("unfading");
-    element.style.display = 'block';
-    var timer = setInterval(function () {
-        if (op >= 1){
-            clearInterval(timer);
-            fade(h1);
+async function test(item){
+    return new Promise((resolve,reject)=>{
+        for(i=0;i<=3000000;i++){
+            if(i==3000000) var flag;
         }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
-    }, 50);
+        resolve(flag);
+    });
 }
-unfade(h1);
+test(h1).then(result=>{
+    console.log('timid');
+});
+var op = 0.1;  // initial opacity
+function unfade(element) {
+        console.log("unfading");
+        element.style.opacity = op;
+        element.style.display = 'block';
+        var timer = setInterval(function () {
+            if (op >= 1){
+                clearInterval(timer);
+                fade(h1);
+            }
+            element.style.opacity = op;
+            element.style.filter.opacity = op * 100 ;
+            op += op * 0.04;
+        }, 50);
+}
+unfade(h1).then((result)=>{
+    console.log("timing!")
+});
 
-async function fade(element) {
-    console.log("waited!")
-    var op = 1;  // initial opacity
+function fade(element) {
     var timer = setInterval(function () {
         if (op <= 0.1){
             clearInterval(timer);
             element.style.display = 'none';
         }
         element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        element.style.filter.opacity = op * 100;
         op -= op * 0.1;
     }, 50);
 }
